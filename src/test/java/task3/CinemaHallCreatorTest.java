abfed161266cd54280cc0aa2351aa7e6dbf4f7ee
osshersh <olegshershnov@gmail.com>
2023-01-14 14:32:22 +0100

    Zrobiono task3

---
+++ src/test/java/task3/CinemaHallCreatorTest.java
@@ -0,0 +1,34 @@
+package task3;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CinemaHallCreatorTest {
+    CinemaHallCreator cinemaHall;
+    PrintStream consoleStream;
+    ByteArrayOutputStream outputStream;
+    PrintStream stream;
+
+    @BeforeEach
+    void init() {
+        cinemaHall = new CinemaHallCreator();
+        consoleStream = System.out;
+        outputStream = new ByteArrayOutputStream();
+        stream = new PrintStream(outputStream);
+        System.setOut(stream);
+    }
+
+    @Test
+    void shouldPrintCinemaHallsByType() {
+        cinemaHall.printCinemaHallsByType();
+        System.setOut(consoleStream);
+
+        assertEquals("4D: [Yellow Hall, Purple Hall, Silver Hall]\r\n" +
+                "3D: [Red Hall, Green Hall, Orange Hall, Diamond Hall]\r\n" +
+                "2D: [Blue Hall, White Hall, Turkish Hall]\r\n", outputStream.toString());
+    }
+}
